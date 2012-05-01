b5e40b9194750f1d7af3f58cdcd9da01797b7237
Lucas Panjer <lucas.panjer@tasktop.com>
2012-05-01 12:09:44 -0700

    copyrights

--- com.tasktop.c2c.server/com.tasktop.c2c.server.common.profile.web.ui/src/main/java/com/tasktop/c2c/server/common/profile/web/shared/actions/UpdateOrganizationAction.java
+++ com.tasktop.c2c.server/com.tasktop.c2c.server.common.profile.web.ui/src/main/java/com/tasktop/c2c/server/common/profile/web/shared/actions/UpdateOrganizationAction.java
@@ -1,6 +1,5 @@
 /*******************************************************************************
  * Copyright (c) 2010, 2012 Tasktop Technologies
- * Copyright (c) 2010, 2011 SpringSource, a division of VMware
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
