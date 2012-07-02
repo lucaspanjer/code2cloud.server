d49c9fbea1e90fdd1cd96c9b836679a9eb473701
Clint Morgan <clint.morgan@tasktop.com>
2012-07-02 11:55:09 -0700

    remove old/unused files

--- com.tasktop.c2c.server/com.tasktop.c2c.server.configuration.service/alm-home/bin/initMysql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir /home/cloudalm/mysql
-chown -R mysql /home/cloudalm/mysql
-chgrp -R mysql /home/cloudalm/mysql
-cp -Rp /opt/cloudalm/configuration/mysql-template/* /home/cloudalm/mysql
-
-/etc/init.d/mysql start
-
