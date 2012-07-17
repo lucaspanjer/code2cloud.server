368e2364affaf5aeeb1c8061565ec613c083df81
Lucas Panjer <lucas.panjer@tasktop.com>
2012-07-17 13:03:35 -0700

    update default database configuration script to fix grants

--- com.tasktop.c2c.server/com.tasktop.c2c.server.developer.support/configuration/local/configureDatabases.sql
+++ com.tasktop.c2c.server/com.tasktop.c2c.server.developer.support/configuration/local/configureDatabases.sql
@@ -2,8 +2,8 @@ create database bugstest_tasks;
 create database profile;
 
 create user 'bugsTester'; GRANT ALL PRIVILEGES ON *.* TO 'bugsTester'@'localhost';
-create user 'bugs' identified by 'bugs'; GRANT ALL PRIVILEGES ON *.* TO 'bugs'@'localhost';
-create user 'wiki' identified by 'wiki'; GRANT ALL PRIVILEGES ON *.* TO 'calm'@'localhost';
+create user 'tasks' identified by 'tasks'; GRANT ALL PRIVILEGES ON *.* TO 'tasks'@'localhost';
+create user 'wiki' identified by 'wiki'; GRANT ALL PRIVILEGES ON *.* TO 'wiki'@'localhost';
 create user 'profile' identified by 'profile'; GRANT ALL PRIVILEGES ON *.* TO 'profile'@'localhost';
 commit; 
 flush privileges;
