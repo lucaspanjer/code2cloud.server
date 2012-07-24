c86db35984a0e58ea8a188961109a05c81527432
Lucas Panjer <lucas.panjer@tasktop.com>
2012-07-24 13:59:12 -0700

    4448: format
    Task-Url: https://q.tasktop.com/alm/#projects/c2c/task/4448

--- com.tasktop.c2c.server/com.tasktop.c2c.server.configuration.service/src/main/java/com/tasktop/c2c/server/configuration/service/ProjectServiceConfiguration.java
+++ com.tasktop.c2c.server/com.tasktop.c2c.server.configuration.service/src/main/java/com/tasktop/c2c/server/configuration/service/ProjectServiceConfiguration.java
@@ -19,26 +19,36 @@ public class ProjectServiceConfiguration {
 
 	/** Property name for the project identifier. */
 	public static final String PROJECT_ID = "profile.application.identifier";
+
 	/** Property name for the organization identifier, if present. */
 	public static final String ORGANIZATION_ID = "profile.organization.identifier";
+
 	/** Property name for an identifer that will always be unique (even after deletion). */
 	public static final String UNIQUE_IDENTIFER = "profile.project.identifier.unique";
+
 	/** Property name for the profile protocol. */
 	public static final String PROFILE_PROTOCOL = "profile.protocol";
+
 	/** Property name for the profile hostname. */
 	public static final String PROFILE_HOSTNAME = "profile.hostname";
-	/** Property name for the profile base serivce url for the given project (up to the ../s/{projectId}/). */
+
+	/** Property name for the profile base service URL for the given project (up to the ../s/{projectId}/). */
 	public static final String PROFILE_BASE_SERVICE_URL = "profile.service.path";
-	/** Property name for the base profile url. */
+
+	/** Property name for the base profile URL. */
 	public static final String PROFILE_BASE_URL = "profile.url";
-	/** Property Name of the git repository to be created. */
+
+	/** Property name of the git repository name to be created. */
 	public static final String APPLICATION_GIT_PROPERTY = "application.git.name";
-	/** Property Name of the git repository to be created. */
+
+	/** Property name of the git repository URL to be created. */
 	public static final String APPLICATION_GIT_URL = "application.git.url";
+
 	/** Required properties to be sent when configuring a node. */
 	public static final String[] REQUIRED_PROPERTIES = new String[] { ProjectServiceConfiguration.PROJECT_ID,
 			ProjectServiceConfiguration.PROFILE_PROTOCOL, ProjectServiceConfiguration.PROFILE_HOSTNAME,
 			ProjectServiceConfiguration.PROFILE_BASE_SERVICE_URL, ProjectServiceConfiguration.PROFILE_BASE_URL };
+
 	public static final String MARKUP_LANGUAGE = "markup.language";
 
 	private String projectIdentifier;
