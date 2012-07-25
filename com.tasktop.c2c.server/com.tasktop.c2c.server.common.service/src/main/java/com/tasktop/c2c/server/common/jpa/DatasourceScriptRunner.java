59590081415250b29ab3a63dd738ec2ba968889d
Clint Morgan <clint.morgan@tasktop.com>
2012-07-25 11:30:40 -0700

    Fix copyright

--- com.tasktop.c2c.server/com.tasktop.c2c.server.common.service/src/main/java/com/tasktop/c2c/server/common/jpa/DatasourceScriptRunner.java
+++ com.tasktop.c2c.server/com.tasktop.c2c.server.common.service/src/main/java/com/tasktop/c2c/server/common/jpa/DatasourceScriptRunner.java
@@ -1,6 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2012, Oracle and/or its affiliates. 
- * All rights reserved. 
+ * Copyright (c) 2010, 2012 Tasktop Technologies
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Tasktop Technologies - initial API and implementation
  ******************************************************************************/
 package com.tasktop.c2c.server.common.jpa;
 
