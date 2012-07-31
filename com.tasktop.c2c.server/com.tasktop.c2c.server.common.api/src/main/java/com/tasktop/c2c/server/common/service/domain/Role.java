a5ce56c3be69b0eaf5224d7c9fdf4b2bb3ac048b
Lucas Panjer <lucas.panjer@tasktop.com>
2012-07-30 17:04:48 -0700

    improve Role documentation

--- com.tasktop.c2c.server/com.tasktop.c2c.server.common.api/src/main/java/com/tasktop/c2c/server/common/service/domain/Role.java
+++ com.tasktop.c2c.server/com.tasktop.c2c.server.common.api/src/main/java/com/tasktop/c2c/server/common/service/domain/Role.java
@@ -18,14 +18,36 @@ public final class Role {
 		// no instantiation of this class.
 	}
 
+	/**
+	 * A user who is not authenticated.
+	 */
 	public static final String Anonymous = "ROLE_ANONYMOUS";
+
+	/**
+	 * A role used for user that executes system actions.
+	 */
 	public static final String System = "ROLE_SYSTEM";
+
+	/**
+	 * An admin user.
+	 */
 	public static final String Admin = "ROLE_ADMIN";
-	/** A Logged-in user that is not necessarily a member of a project, but has access. */
+
+	/**
+	 * An authenticated user that is not necessarily a member of a project, but has access. (e.g., a watcher.)
+	 */
 	public static final String Community = "ROLE_COMMUNITY";
-	/** An anon user that has read only access to a project. */
+
+	/**
+	 * An authenticated user that has read-only access to a project.
+	 */
 	public static final String Observer = "ROLE_OBSERVER";
+
+	/**
+	 * An authenticated user. In project context, an authenticated user who is a member of the project.
+	 */
 	public static final String User = "ROLE_USER";
+
 	/**
 	 * This is a User who has pending legal agreements. As such the permissions are restricted to just agreeing to
 	 * agreements.
