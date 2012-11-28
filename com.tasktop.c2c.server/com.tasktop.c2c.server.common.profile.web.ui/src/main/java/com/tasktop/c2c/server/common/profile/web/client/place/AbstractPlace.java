6e37a39f4dba922a10995cdc1d7a8e384b5fb67c
Clint Morgan <clint.morgan@tasktop.com>
2012-11-28 11:18:46 -0800

    5022: Breadcrumbs links are broken from Hudson Fix test. Task-URL: https://q.tasktop.com/alm/#projects/c2c/task/5022

--- com.tasktop.c2c.server/com.tasktop.c2c.server.common.profile.web.ui/src/main/java/com/tasktop/c2c/server/common/profile/web/client/place/AbstractPlace.java
+++ com.tasktop.c2c.server/com.tasktop.c2c.server.common.profile.web.ui/src/main/java/com/tasktop/c2c/server/common/profile/web/client/place/AbstractPlace.java
@@ -12,9 +12,9 @@
  ******************************************************************************/
 package com.tasktop.c2c.server.common.profile.web.client.place;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.http.client.UrlBuilder;
 import com.google.gwt.place.shared.Place;
-import com.google.gwt.user.client.Window;
 import com.tasktop.c2c.server.common.profile.web.client.AuthenticationHelper;
 import com.tasktop.c2c.server.common.profile.web.client.ProfileGinjector;
 import com.tasktop.c2c.server.common.web.client.navigation.Path;
@@ -46,7 +46,15 @@ public abstract class AbstractPlace extends Place implements IPlace {
 	public abstract String getPrefix();
 
 	public String getHref() {
-		UrlBuilder urlBuilder = Window.Location.createUrlBuilder();
+
+		if (!GWT.isClient()) {
+			// For tests
+			return "http://localhost/#" + getHistoryToken();
+		}
+		UrlBuilder urlBuilder = new UrlBuilder();
+		urlBuilder.setProtocol("http");
+		urlBuilder.setHost("localhsot");
+
 		urlBuilder.setPath(Path.getBasePath());
 		return urlBuilder.setHash(getHistoryToken()).buildString();
 	}
