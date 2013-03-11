8c64cb60122a0909e3a773575ea2d649b927f8b5
Michael Nelson <michael.nelson@tasktop.com>
2013-03-11 15:37:30 -0700

    5012: Upgrade to GWT 2.5
    Change-Id: I1eb53c75a2148411e83e104d27fdeea692a24afb
    Task-URL: https://q.tasktop.com/alm/#projects/c2c/task/5012
    Reviewed-on: https://q.tasktop.com/alm/s/c2c/gerrit/252
    Reviewed-by: Clint Morgan <clint.morgan@tasktop.com>
    Reviewed-by: Michael Nelson <michael.nelson@tasktop.com>
    Tested-by: Michael Nelson <michael.nelson@tasktop.com>

---
+++ com.tasktop.c2c.server/com.tasktop.c2c.server.common.web/src/main/java/com/google/gwt/core/client/GWTBridge.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2013 Tasktop Technologies
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Tasktop Technologies - initial API and implementation
+ ******************************************************************************/
+package com.google.gwt.core.client;
+
+/**
+ * HACK http://code.google.com/p/google-web-toolkit/issues/detail?id=7527
+ */
+public abstract class GWTBridge extends com.google.gwt.core.shared.GWTBridge {
+
+}
